4d41f314150da55c762809ef1cd23fbdb31da845
CaoHoaiTan <caohoaitan@gmail.com>
2021-07-02 21:08:23 +0700

    Push

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Mọi người à!',
+    introDesc: `Điều kiện chọn người yêu của Hoàng chỉ có một. Phải có nụ cười đẹp như Hoàng vậy đó!!!`,
+    btnIntro: '🤩🤩🤩',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn Hoàng đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười của Hoàng ',
+    btnYes: 'Hoàng đẹp troaiii nhứt nách <33',
+    btnNo: 'Không, Hoàng xấu bome à :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Hoàng <3',
+    btnReply: 'Gửi cho Hoàng <3',
+    reply: 'Vì nụ cười của Hoàng đẹp nhứttt trên đờiiiii <33333333',
+    mess: 'Hoàng biết mà 🥰. Yêu nhiều nhiều 😘😘',
+    messDesc: 'Hẹn nhau một ngày gần nhất nhé!!!.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100011774670969' //link mess của các bạn.
+}
